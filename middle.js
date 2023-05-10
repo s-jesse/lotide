977c266dea2358851b99b60e7bb6c57c5ffa0766
Jesse Sandhu <s.jesse23@gmail.com>
2023-05-10 00:26:59 -0700

    updated middle.js to incorporate assertArraysEqual and middle function with export/require and inside testing folder with other updated functions

--- middle.js
+++ middle.js
@@ -1,24 +1,27 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
+const eqArrays = require('./eqArrays')
+const assertArraysEqual = require('./assertArraysEqual');
 
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
+// const eqArrays = function(arr1, arr2) {
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
 
-  }
-  return true;
-};
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[i] !== arr2[i]) {
+//       return false;
+//     }
 
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2) === true) {
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-  } else if (array1 !== array2) {
-    console.log(`❗️❗️❗️ Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
+//   }
+//   return true;
+// };
+
+// const assertArraysEqual = function(array1, array2) {
+//   if (eqArrays(array1, array2) === true) {
+//     console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+//   } else if (array1 !== array2) {
+//     console.log(`❗️❗️❗️ Assertion Failed: ${array1} !== ${array2}`);
+//   }
+// };
 
 const middle = function(arr) {
 // need to pass arugment thats an array that we need to go through
@@ -38,24 +41,24 @@ const middle = function(arr) {
   const middleNumber = arr[middleIndex];
   return [middleNumber];
 };
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
-// initial test console.log(middle([1, 2, 3, 4, 5, 6]));
+// console.log(middle([1])); // => []
+// console.log(middle([1, 2])); // => []
+// console.log(middle([1, 2, 3])); // => [2]
+// console.log(middle([1, 2, 3, 4, 5])); // => [3]
+// console.log(middle([1, 2, 3, 4])); // => [2, 3]
+// console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+// // initial test console.log(middle([1, 2, 3, 4, 5, 6]));
 
-// test with assertArraysequal function
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+// // test with assertArraysequal function
+// assertArraysEqual(middle([1]), []);
+// assertArraysEqual(middle([1, 2, 3]), [2]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
 /*
 when writing notes take in pseudocode
 look at how we added code to arr parameter without needing seperate var
 look at how we utilized comparative / math operations to the array parameter
 add in logical notes to understand the entire process
 */
-
+module.exports = middle
