0a4cc1723c16189d40a65db29d59689a448e181a
Jesse Sandhu <s.jesse23@gmail.com>
2023-05-13 00:52:58 -0700

    updated with export

--- map.js
+++ map.js
@@ -1,27 +1,31 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2) === true) {
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-  } else if (array1 !== array2) {
-    console.log(`❗️❗️❗️ Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual');
+const eqArrays = require('./eqArrays');
 
 
-const words = ["ground", "control", "to", "major", "tom"];
+// const eqArrays = function(arr1, arr2) {
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
+
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[i] !== arr2[i]) {
+//       return false;
+//     }
+
+//   }
+//   return true;
+// };
+
+// const assertArraysEqual = function(array1, array2) {
+//   if (eqArrays(array1, array2) === true) {
+//     console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+//   } else if (array1 !== array2) {
+//     console.log(`❗️❗️❗️ Assertion Failed: ${array1} !== ${array2}`);
+//   }
+// };
+
+
+//const words = ["ground", "control", "to", "major", "tom"];
 
 // const map = function(arr, callBack) {
 // //The map function will return a new array based on the results of the callback function.
@@ -53,12 +57,14 @@ const map = function(array, callback) {
 // using a simple template to console.log
 // see that function is structured correctly
 
-const results1 = map(words, word => word[0]);
-const results2 = map(words, word => word[1]);
+// const results1 = map(words, word => word[0]);
+// const results2 = map(words, word => word[1]);
 
-//call back function is showing value of index 0
-//console.log(results1);
-// test calling the assertArraysEqual function and puttin in the assertion parameters
+// //call back function is showing value of index 0
+// //console.log(results1);
+// // test calling the assertArraysEqual function and puttin in the assertion parameters
 
-assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']); // should pass
-assertArraysEqual(results2, ['g', 'c', 't', 'm', 't']); //  should fail
+// assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']); // should pass
+// assertArraysEqual(results2, ['g', 'c', 't', 'm', 't']); //  should fail
+
+module.exports = map
