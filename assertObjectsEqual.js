0d92cdc39160d03628ab138cf94546a0f554eed1
Jesse Sandhu <s.jesse23@gmail.com>
2023-05-13 00:50:35 -0700

    updated with export

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,24 +1,26 @@
-const eqObjects = function(object1, object2) {
-  let obj1Keys = Object.keys(object1);
-  let obj2Keys = Object.keys(object2);
-  if (obj1Keys.length !== obj2Keys.length) {
-    return false;
-  }
-  for (const key of obj1Keys) {
-    //console.log("key", key);
-    let object1Value = object1[key];
-    //console.log("values of obj1[key]: ", object1Value);
-    let object2Value = object2[key];
-    //console.log("object2Value", object2Value);
-    if (Array.isArray(object1Value) && Array.isArray(object2Value)) {
-      //eqObjects(object1Value, object2Value);
-      return true;
-    } else if (object1Value !== object2Value) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqObjects = require('./eqObjects');
+
+// const eqObjects = function(object1, object2) {
+//   let obj1Keys = Object.keys(object1);
+//   let obj2Keys = Object.keys(object2);
+//   if (obj1Keys.length !== obj2Keys.length) {
+//     return false;
+//   }
+//   for (const key of obj1Keys) {
+//     //console.log("key", key);
+//     let object1Value = object1[key];
+//     //console.log("values of obj1[key]: ", object1Value);
+//     let object2Value = object2[key];
+//     //console.log("object2Value", object2Value);
+//     if (Array.isArray(object1Value) && Array.isArray(object2Value)) {
+//       //eqObjects(object1Value, object2Value);
+//       return true;
+//     } else if (object1Value !== object2Value) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
 
 const assertObjectsEqual = function(obj1, obj2) {
   const inspect = require('util').inspect;
@@ -29,15 +31,17 @@ const assertObjectsEqual = function(obj1, obj2) {
   }
 };
 
-const car1 = {
-  color: "red", model:"truck", condition: "old"
-};
+// const car1 = {
+//   color: "red", model:"truck", condition: "old"
+// };
 
-const car2 = {
-  color: "red", model:"van", condition: "old"
-};
-const car3 = {
-  color: "red", model:"van", condition: "old"
-};
-assertObjectsEqual(car1, car2); // should fail: false
-assertObjectsEqual(car2, car3); // should pass: true
+// const car2 = {
+//   color: "red", model:"van", condition: "old"
+// };
+// const car3 = {
+//   color: "red", model:"van", condition: "old"
+// };
+// assertObjectsEqual(car1, car2); // should fail: false
+// assertObjectsEqual(car2, car3); // should pass: true
+
+module.exports = assertObjectsEqual;
