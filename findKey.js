a5bf1a27f79b6528e57569ac08eb9830d5943cd9
Jesse Sandhu <s.jesse23@gmail.com>
2023-05-21 16:12:59 -0700

    updated

--- findKey.js
+++ findKey.js
@@ -1,26 +1,11 @@
+// findKey function should search for a key through the object using a callback function to return said key once it matches with a truthy value.
+
 const findKey = function(object, callback) {
-  // const keyList = Object.keys(object);
-  // console.log("object.key", keyList);
 
-  // for (let key in object) {
-
-  //console.log("key loop", object[key]);
-  //let value = object[key]; same thing as object[key] directly in callback function
-  //console.log("keyValues loop", value);
-
-  //     if (callback(object[key])) {
-  //       //console.log("key: ", key);
-  //       return key;
-  //     }
-  //   }
-  // };
   for (const prop in object) {
-    //console.log("object loop: ", prop);
-    //objectProp = object[prop];
-    //console.log("property loop: ", objectProp);
+
 
     if (callback(object[prop])) {
-      //console.log("return value");
       return prop;
     } else if (callback(object[prop] !== prop)) {
       return undefined;
@@ -28,33 +13,4 @@ const findKey = function(object, callback) {
   }
 };
 
-// for (const keyValues in objectProp){
-//   values = objectProp[keyValues];
-//   console.log("key loop: ", keyValues);
-//   console.log("value loop: ", values);
-//}
-// const keyList = Object.keys(object);
-// console.log("object.key", keyList);
-// for (let key of keyList) {
-//   console.log("key loop", key);
-
-// //Output Example
-// assertEqual(findKey({
-//   "Blue Hill": { stars: 1 },
-//   "Akaleri":   { stars: 3 },
-//   "noma":      { stars: 2 },
-//   "elBulli":   { stars: 3 },
-//   "Ora":       { stars: 2 },
-//   "Akelarre":  { stars: 3 }
-// }, x => x.stars === 2), "noma"); // => "noma"
-
-// assertEqual(findKey({
-//   "Blue Hill": { stars: 1 },
-//   "Akaleri":   { stars: 3 },
-//   "noma":      { stars: 2 },
-//   "elBulli":   { stars: 3 },
-//   "Ora":       { stars: 2 },
-//   "Akelarre":  { stars: 3 }
-// }, x => x.stars === 2), "Blue Hill"); // => "noma"
-
-module.exports = findKey
+module.exports = findKey;
