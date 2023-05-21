39b6060aebcf4d0486cc514cfbefd62c4cee72b4
Jesse Sandhu <s.jesse23@gmail.com>
2023-05-21 13:58:21 -0700

    updated

--- flatten.js
+++ flatten.js
@@ -1,30 +1,3 @@
-const eqArrays = require('./eqArrays');
-const assertArraysEqual = require('./assertArraysEqual');
-
-
-
-// const eqArrays = function(arr1, arr2) {
-//   if (arr1.length !== arr2.length) {
-//     return false;
-//   }
-
-//   for (let i = 0; i < arr1.length; i++) {
-//     if (arr1[i] !== arr2[i]) {
-//       return false;
-//     }
-
-//   }
-//   return true;
-// };
-
-// const assertArraysEqual = function(array1, array2) {
-//   if (eqArrays(array1, array2) === true) {
-//     console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-//   } else if (array1 !== array2) {
-//     console.log(`❗️❗️❗️ Assertion Failed: ${array1} !== ${array2}`);
-//   }
-// };
-
 const flatten = function(arr) {
 // created flatten function to take a array paremter
   const newArr = [];
