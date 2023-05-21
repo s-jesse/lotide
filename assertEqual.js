caf0e2585710ab7b3ae4bb8d0e21b83a10d5503b
Jesse Sandhu <s.jesse23@gmail.com>
2023-05-21 16:11:17 -0700

    updated

--- assertEqual.js
+++ assertEqual.js
@@ -1,3 +1,5 @@
+// assertEqual function should compare primitive type values and should display assertion if it passes or fails.
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -7,31 +9,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// // Test Code
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-
-/* \
-input
-[x] take two values
-
-output
-[x] print out if they or not
-
-Process / Rules
-- values should match and print (console.log) Assertion Passed:
-[actual] === [expected] with values inside
-- if not then print (console.log) Assertion Failed:
-[actual] !== [expected]
-*/
-
-// examples Lighthouse Labs !== Bootcamp (strings dont match)
-
-/*
-Test
-[x] Compares indentical string
-[x] Compares non-indentical string
-[x] Compares indentical numbers
-[x] Compares non-indentical numbers
-*/
 module.exports = assertEqual;
