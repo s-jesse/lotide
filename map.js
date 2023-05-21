2d4e7fc229d924e04f3bac1540d2141f98e49e14
Jesse Sandhu <s.jesse23@gmail.com>
2023-05-21 16:13:56 -0700

    updated

--- map.js
+++ map.js
@@ -1,30 +1,13 @@
+// map function should take an array to map and return new array based on results of callback function.
 const map = function(array, callback) {
-  // temporary code:
-  // console.log('array: ', array);
-  // console.log('callback: ', callback);
+
   const results = [];
 
   for (let item of array) {
-    // console.log('item BEFORE: ', item);
-    // console.log('item AFTER: ', callback(item));
-    // console.log('---');
+
     results.push(callback(item));
   }
-  // adding in loop / consoles to go through each element
-  // then showing each item after putting through call back function
   return results;
 };
-// using a simple template to console.log
-// see that function is structured correctly
 
-// const results1 = map(words, word => word[0]);
-// const results2 = map(words, word => word[1]);
-
-// //call back function is showing value of index 0
-// //console.log(results1);
-// // test calling the assertArraysEqual function and puttin in the assertion parameters
-
-// assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']); // should pass
-// assertArraysEqual(results2, ['g', 'c', 't', 'm', 't']); //  should fail
-
-module.exports = map
+module.exports = map;
