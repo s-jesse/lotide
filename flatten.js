a9fe189c0ea2e47a3a9dbab30ab0e3967c03e7be
Jesse Sandhu <s.jesse23@gmail.com>
2023-05-21 16:13:09 -0700

    updated

--- flatten.js
+++ flatten.js
@@ -1,34 +1,24 @@
+// flatten function should "flatten" nested arrays into single array.
+
 const flatten = function(arr) {
-// created flatten function to take a array paremter
+
   const newArr = [];
-  // created a new array var to access / store value later
+
   for (let i = 0; i < arr.length; i++) {
-    // loop to go through array parameter
     const loopedElement = arr[i];
     // created variable to store array paremeter with the array loop running through it
     if (!Array.isArray(loopedElement)) {
-      // if statement to reverse (false) to see if element is array in loopedElement variable with arr[i]
       newArr.push(loopedElement);
-      // taking the elements that are false and pushing into newArr created before for loop
     } else {
-      // need else statement deal with arrays that [i] loop will encounter
       for (let j = 0; j < loopedElement.length; j++) {
       // create second loop (child loop to [i] loop) using loopedElement.length to run inside nested arrays
         newArr.push(loopedElement[j]);
-      // since this assingment is not going any further with complexity, we assume its numbers inside and dont need any further statements
+      // since this assignment is not going any further with complexity, we assume its numbers inside and dont need any further statements
       // push nestled array elements into same newArr
       }
     }
   }
   return newArr;
 };
-//tips
-// first loop more like parent arr
-// second loop more like child running alongside
 
-// test
-// flatten([1, 2, [3, 4], 5, [6]]); // => [1, 2, 3, 4, 5, 6]
-// // assertion test using input with flatten function and comparing to output needed
-// assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]),[1, 2, 3, 4, 5, 6]);
-
-module.exports = flatten
+module.exports = flatten;
